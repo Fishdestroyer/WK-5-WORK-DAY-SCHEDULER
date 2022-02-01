40b99ec0497867aebee13446cb0644a6775e3b50
Fishdestroyer <jeffwhitner@gmail.com>
2022-01-31 21:47:52 -0600

    additions to index.js,

--- develop/index.js
+++ develop/index.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
         localStorage.setItem(time, plannerText);// Saves to local storage
 
     }); 
-
+    //Keeps the description showing on the HTML screen, if not defined, will save in local storage but will not show
+    //$("#5 .description").val(localStorage.getItem("5")); Expected results= 5 AM will save to Local but not display on refresh
     $("#6 .description").val(localStorage.getItem("6"));
     $("#7 .description").val(localStorage.getItem("7"));
     $("#8 .description").val(localStorage.getItem("8"));
@@ -23,3 +24,13 @@ $(document).ready(function(){
     $("#16 .description").val(localStorage.getItem("16"));
     $("#17 .description").val(localStorage.getItem("17"));
     $("#18 .description").val(localStorage.getItem("18"));
+    // add more rows for more time
+    function timeChange(){
+        var currentHour = moment().hour(); // Number
+        
+        $('.time-block').each(function(){
+            var idTime = parseInt($(this).attr("id"));// Defines which hour the attribute is
+         
+
+
+});
